b4de29be601d44993ed78b764a4662b67e4344fa
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-21 18:22:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function add(){
+    document.getElementById('error').innerHTML = "";
+
+    let name = document.getElementById('name').value;
+    if(name == ''){
+        document.getElementById('error').innerHTML = "Please Enter Value";
+    }else{
+        let box = document.getElementById('box');
+        let li = document.createElement('li');
+        li.textContent = name;
+
+        let a = document.createElement('a');
+        a.textContent = "X";
+        a.href = "javascript:void(0)";
+        a.className = "remove";
+        li.appendChild(a);
+
+        // let pos = box.firstElementChild;
+
+        // if(pos == null){
+            box.appendChild(li);
+        // }else{
+            // box.insertBefore(li, pos);
+        // }
+    }
+
+    document.getElementById('name').value="";
+}
+
+let btn = document.querySelector('ul');
+btn.addEventListener('click', function(e){
+    let box = document.getElementById('box');
+    let li = e.target.parentNode;
+    box.removeChild(li);
+});
